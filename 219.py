fcd0c0adb75e4e327bdc9fd77bfbcad501b67578
subash <subashkj005@gmail.com>
2023-06-15 10:17:16 +0530

    Initial commit

---
+++ 219.py
@@ -0,0 +1,33 @@
+# 219. Contains Duplicate II
+
+# Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+
+
+# Example 1:
+
+# Input: nums = [1,2,3,1], k = 3
+# Output: true
+# Example 2:
+
+# Input: nums = [1,0,1,1], k = 1
+# Output: true
+# Example 3:
+
+# Input: nums = [1,2,3,1,2,3], k = 2
+# Output: false
+
+
+
+class Solution:
+    def containsNearbyDuplicate(self, nums, k):
+        num_dict = {}
+        for i, num in enumerate(nums):
+            if num in num_dict and i - num_dict[num] <= k:
+                return True
+            num_dict[num] = i
+        return False
+
+k=3
+nums = [1,2,3,1]
+x = Solution()
+print(x.containsNearbyDuplicate(nums,k))
